d7d9fc61dc4e54d5179127b06fa9588cdbf676af
Tommy Palmer <hi@tommyp.org>
2025-06-28 14:57:54 +0100

    render goals

--- lib/tracker_web/live/day_live/day_live.ex
+++ lib/tracker_web/live/day_live/day_live.ex
@@ -1,36 +1,37 @@
 defmodule TrackerWeb.DayLive.Show do
+  alias Tracker.Goals
   alias TrackerWeb.Utils.DateHelpers
   use TrackerWeb, :live_view
 
   def handle_params(params, _, socket) do
     socket =
-      if Map.has_key?(params, "date") do
-        try do
-          date = Map.get(params, "date")
-          assign(socket, :date, Date.from_iso8601!(date))
-        rescue
-          ArgumentError ->
-            socket
-            |> push_patch(to: ~p"/")
-        end
-      else
-        assign(socket, :date, Date.utc_today())
-      end
+      socket
+      |> maybe_assign_date(params)
+      |> assign_goals()
 
     {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <div class="grid grid-cols-6 uppercase">
-      <.yesterday_button date={@date} />
+    <header>
+      <div class="flex justify-between md:grid grid-cols-6 uppercase align-baseline">
+        <.yesterday_button date={@date} />
 
-      <div class="col-span-4">
-        <h1 class="text-center">{DateHelpers.humanized!(@date)}</h1>
+        <div class="col-span-4">
+          <h1 class="text-center">{DateHelpers.humanized!(@date)}</h1>
+        </div>
+
+        <.tomorrow_button date={@date} />
       </div>
-
-      <.tomorrow_button date={@date} />
-    </div>
+    </header>
+    <main class="md:grid grid-cols-6">
+      <ul class="flex flex-col gap-y-2 col-span-4 col-start-2 md:px-4 py-4 md:py-2">
+        <li :for={goal <- @goals} class="px-4 py-2 border border-zinc-300">
+          <p class="text-lg">{goal.description}</p>
+        </li>
+      </ul>
+    </main>
     """
   end
 
@@ -90,5 +91,24 @@ defmodule TrackerWeb.DayLive.Show do
      socket
      |> assign(:date, date)
      |> push_patch(to: ~p"/?date=#{date}", replace: true)}
+  end
+
+  defp maybe_assign_date(socket, params) do
+    if Map.has_key?(params, "date") do
+      try do
+        date = Map.get(params, "date")
+        assign(socket, :date, Date.from_iso8601!(date))
+      rescue
+        ArgumentError ->
+          socket
+          |> push_patch(to: ~p"/")
+      end
+    else
+      assign(socket, :date, Date.utc_today())
+    end
+  end
+
+  defp assign_goals(socket) do
+    assign(socket, :goals, Goals.list_goals())
   end
 end
